b987fb373ee119de3a21b3955f45c9da56cb1892
jihoo414-tech <jihoo414@gmail.com>
2026-08-26 10:30:12 +0900

    feat: 목록 최신순으로 조회 기능 구현

--- src/main/java/com/loadbalancing/kiosk/domain/order/order/controller/OrderController.java
+++ src/main/java/com/loadbalancing/kiosk/domain/order/order/controller/OrderController.java
@@ -2,12 +2,15 @@ package com.loadbalancing.kiosk.domain.order.order.controller;
 
 import com.loadbalancing.kiosk.domain.order.order.dto.request.OrderCreateRequest;
 import com.loadbalancing.kiosk.domain.order.order.dto.response.OrderCreateResponse;
+import com.loadbalancing.kiosk.domain.order.order.dto.response.OrderListResponse;
 import com.loadbalancing.kiosk.domain.order.order.service.OrderService;
 import com.loadbalancing.kiosk.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -28,5 +31,18 @@ public class OrderController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(ApiResponse.success(201, response));
+    }
+    //해당 email로 db를 조회하여, 최신 주문 순으로 반환
+    //todo OrderStatus별로 주문 나눠서 보여주기.
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse<List<OrderListResponse>>> getOrderList(
+            @RequestParam String email
+    ){
+        List<OrderListResponse> response =
+                orderService.getOrderList(email);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(200,response)
+        );
     }
 }
--- src/main/java/com/loadbalancing/kiosk/domain/order/order/dto/request/OrderCreateRequest.java
+++ src/main/java/com/loadbalancing/kiosk/domain/order/order/dto/request/OrderCreateRequest.java
@@ -3,7 +3,7 @@ package com.loadbalancing.kiosk.domain.order.order.dto.request;
 import com.loadbalancing.kiosk.domain.order.orderItem.dto.OrderItemRequest;
 
 import java.util.List;
-
+//todo validation으로 검증(email, address postalCode 등)
 public record OrderCreateRequest(
         String email,
         String addressLine1,
