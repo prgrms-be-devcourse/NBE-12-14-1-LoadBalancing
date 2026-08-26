d41992554526a05bd273c783f1ec4de0391d79a9
YoonJaeHwan2041 <jkl0379@naver.com>
2026-08-26 15:17:05 +0900

    feat: 관리자 로그인 페이지 구현 및 토큰 저장 로직 추가

---
+++ client/app/admin/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminOrdersPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p className="text-gray-500">여기에 만드는중 ㅋㅋ</p>
+    </div>
+  );
+}
