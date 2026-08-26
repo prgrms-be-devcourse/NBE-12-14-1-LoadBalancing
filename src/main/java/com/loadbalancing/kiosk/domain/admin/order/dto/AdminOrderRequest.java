3ed7471152b802f362c27d6e099493fbbbb49c09
kimssin1991 <kimsssin1991@naver.com>
2026-08-26 11:03:17 +0900

    feat : AdminOrderStatus 주문상태변경/주문삭제

---
+++ src/main/java/com/loadbalancing/kiosk/domain/admin/order/dto/AdminOrderRequest.java
@@ -0,0 +1,9 @@
+package com.loadbalancing.kiosk.domain.admin.order.dto;
+
+import com.loadbalancing.kiosk.domain.order.entity.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+
+public record AdminOrderRequest(
+        @NotNull OrderStatus status
+) {
+}
